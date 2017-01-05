88a14a557a8d2452df09706d6de2f220405d8598
bhavesh971212 <bhavesh.rakhshiya@gmail.com>
2017-01-05 18:36:18 +0530

    test

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -425,7 +425,7 @@ class User_model extends CI_model
         // if($input['email_mob'] && $input['user_type_id']){
         if($input['email_mob']){
           $email = $input['email_mob'];
-           $utype = $input['user_type_id'];
+           $utype = $input['user_type'];
         }else{
           $email = $input['user_email'];
            $utype = $input['user_type'];
